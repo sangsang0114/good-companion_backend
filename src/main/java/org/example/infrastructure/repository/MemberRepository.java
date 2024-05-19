320841d851be86a56e1f9c2ff73ca6911580d78a
yoon <envy15@skuniv.ac.kr>
2024-05-19 21:04:04 +0900

    [feat] Member 엔티티 추가

---
+++ src/main/java/org/example/infrastructure/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package org.example.infrastructure.repository;
+
+import org.example.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByEmail(String email);
+    Optional<Member> findByEmail(String email);
+}
