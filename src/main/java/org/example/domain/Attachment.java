b79a77f6218183a2fcb68eac532377e35a08e1fa
yoon <envy15@skuniv.ac.kr>
2024-05-27 09:32:00 +0900

    [feat] 첨부파일 삭제 여부 플래그 추가

--- src/main/java/org/example/domain/Attachment.java
+++ src/main/java/org/example/domain/Attachment.java
@@ -5,12 +5,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "attachment")
 @Getter
 @Entity
+@SQLDelete(sql = "UPDATE shopdb.attachment SET is_available = 0 WHERE id = ?")
+@SQLRestriction("is_available = 1")
+@DynamicInsert
 public class Attachment extends BaseTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,6 +34,9 @@ public class Attachment extends BaseTime {
 
     @Column(name = "stored_filedname")
     private String storedFilename;
+
+    @Column(name = "is_available")
+    private Integer isAvailable;
 
     @Builder
     public Attachment(String serviceName, Long serviceTarget, String originalFilename, String storedFilename) {
